c7b9cb9e8bd4a0d350a5fcebc00bf3961e1927bc
Ertony_bahil <ertonylbasil@gmail.com>
2024-07-03 18:16:19 +0000

    First way of calculation the chebychev function

--- application.py
+++ application.py
@@ -1,9 +1,24 @@
-def chebyshev_distance(p1, p2):
-    """Docstring..."""
-    return max(abs(x - y) for x, y in zip(p1, p2))
+# def chebyshev_distance(p1, p2):
+#     """Docstring..."""
+#     return max(abs(x - y) for x, y in zip(p1, p2))
+
+# if __name__ == "__main__":
+#     point1 = (1, 2, 3)
+#     point2 = (4, 5, 6)
+#     distance = chebyshev_distance(point1, point2)
+#     print("Chebyshev Distance:", distance)
+
+
+def chebyshev_distance_way1(p1, p2):
+    """Calculate the Chebyshev distance between two points."""
+    max_diff = 0
+    for x, y in zip(p1, p2):
+        max_diff = max(max_diff, abs(x - y))
+    return max_diff
 
 if __name__ == "__main__":
     point1 = (1, 2, 3)
     point2 = (4, 5, 6)
-    distance = chebyshev_distance(point1, point2)
+    distance = chebyshev_distance_way1(point1, point2)
     print("Chebyshev Distance:", distance)
+
